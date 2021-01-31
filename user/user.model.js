6cece580b955f6a8a7a8c8053504f3e9e271aa18
johnjohnn <conde2010@aol.com>
2021-01-31 01:57:41 -0500

    adding seeds

--- user/user.model.js
+++ user/user.model.js
@@ -1,14 +1,5 @@
 const db = require("../database/config")
 
-async function add(user) {
-	const [id] = await db("users").insert(user)
-	return findById(id)
-}
-
-function find() {
-	return db("users").select("id", "username")
-}
-
 function findBy(filter) {
 	return db("users")
 		.select("id", "username", "password")
@@ -28,3 +19,4 @@ module.exports = {
 	findBy,
 	findById,
 }
+
