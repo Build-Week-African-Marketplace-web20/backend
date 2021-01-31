c3ee214f1d4276642f31b62b71fcfeccab63c391
johnjohnn <conde2010@aol.com>
2021-01-30 22:34:25 -0500

    adding packages

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const welcomeRouter =  require("")
+
+
+
+
+
+const server = express()
+const port =  process.env.PORT || 5000  
+
+
+server.use (express.json())
+
+server.use(welcomeRouter)
+
+
+
+
+
+server.use((err, req, res, next) =>{
+    console.log(err)
+    res.status(500).json({
+        message: "something went wrong"
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Running at http://localhost:${port}`)
+})
+// almost there 
